5ae7a037303de88e4cc49c6fbb01098ade9d5263
unknown <gadalai426@gmail.com>
2023-12-08 08:02:03 +0800

    d

---
+++ Piece.cpp
@@ -0,0 +1,24 @@
+
+#include "Piece.h"
+
+Piece::Piece(PieceType pType, PieceColor pColor)
+{
+	type = pType;
+	color = pColor;
+}
+
+Piece::Piece(const Piece &piece)
+{
+	this->type = piece.type;
+	this->color = piece.color;
+}
+
+PieceType Piece::getType()
+{
+	return type;
+}
+
+PieceColor Piece::getColor()
+{
+	return color;
+}
